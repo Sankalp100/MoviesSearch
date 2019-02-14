59b58aa43878787308314e904a428359f5821601
Sankalp100 <sankalpgour123@gmail.com>
2019-02-14 23:28:19 +0530

    update

--- movies_search/src/App.js
+++ movies_search/src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav class="nav-wrapper transparent">
+          <div class="container">
+                  <a href="#" class="brand-logo indigo-text text-darken-4">
+                    <i class="logo material-icons">cloud</i>
+                    MoviesSearcher
+                  </a>
+                  <a href="#" class="sidenav-trigger" data-target="mobile-menu">
+                      <i class="material-icons">menu</i>
+                  </a>
+                  <ul class="right hide-on-med-and-down">
+                      <li><a href="#photos">Photo's</a></li>
+                      <li><a href="#services">Services</a></li>
+                      <li><a href="#contact">Contact</a></li>
+                      <li><a href="#" class="tooltipped btn-floating btn-small indigo darken-4" data-tooltip="This is Instagram">
+                          <i class="fab fa-instagram"></i>
+                      </a></li>
+                      <li><a href="#" class="tooltipped btn-floating btn-small indigo darken-4" data-tooltip="This is Facebook">
+                          <i class="fab fa-facebook"></i>
+                      </a></li>
+                      <li><a href="#" class="tooltipped btn-floating btn-small indigo darken-4" data-tooltip="This is Tiwtter">
+                          <i class="fab fa-twitter"></i>
+                      </a></li>
+                  </ul>
+                  <ul class="sidenav gray lighten-2" id="mobile-menu">
+                      <li><a href="#">Photo's</a></li>
+                      <li><a href="#">Services</a></li>
+                      <li><a href="#">Contact</a></li>
+                  </ul>
+              </div>
+        </nav>
+        
       </div>
     );
   }
